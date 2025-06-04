9d668e4eee31280c41272b48b78fde1443ba0203
Murga <sheikabu1605@gmail.com>
2025-06-05 00:46:19 +0530

    Update footer.php

--- application/views/home/footer.php
+++ application/views/home/footer.php
@@ -1,14 +1,29 @@
+<!-- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100" preserveAspectRatio="none">
+  <g fill="#FFEFD5" transform="scale(1, -1) translate(0, -100)">
+    <path d="M0 0v99.7C62 69 122.4 48.7 205 66c83.8 17.6 160.5 20.4 240-12 54-22 110-26 173-10a392.2 392.2 0 0 0 222-5c55-17 110.3-36.9 160-27.2V0H0Z" opacity=".5"></path>
+    <path d="M0 0v74.7C62 44 122.4 28.7 205 46c83.8 17.6 160.5 25.4 240-7 54-22 110-21 173-5 76.5 19.4 146.5 23.3 222 0 55-17 110.3-31.9 160-22.2V0H0Z"></path>
+  </g>
+</svg> -->
+
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100">
+  <g fill="#FFEFD5" transform="scale(1, -1) translate(0, -100)">
+    <path d="M0 0v99.7C62 69 122.4 48.7 205 66c83.8 17.6 160.5 20.4 240-12 54-22 110-26 173-10a392.2 392.2 0 0 0 222-5c55-17 110.3-36.9 160-27.2V0H0Z" opacity=".5"></path>
+    <path d="M0 0v74.7C62 44 122.4 28.7 205 46c83.8 17.6 160.5 25.4 240-7 54-22 110-21 173-5 76.5 19.4 146.5 23.3 222 0 55-17 110.3-31.9 160-22.2V0H0Z"></path>
+  </g>
+</svg>
+
 <footer class="footer">
+
   <div class="footer-container">
     <!-- About BharatVivaha -->
     <div class="footer-column">
-      <h4>About Us</h4>
+      <h2>About Us</h2>
       <p>BharatVivaha is a trusted matrimony service connecting hearts across communities with a secure and seamless platform for life partner search.</p>
     </div>
 
     <!-- Quick Links -->
     <div class="footer-column">
-      <h4>Quick Links</h4>
+      <h2>Quick Links</h2>
       <ul>
         <!-- <li><a href="/about-us">About</a></li> -->
         <li><a href="/privacy-policy">Privacy Policy</a></li>
@@ -19,25 +34,25 @@
 
      <!-- Contact Info with Inline Icons -->
     <div class="footer-column">
-      <h4>Contact</h4>
+      <h2>Contact</h2>
       <p>
-        <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4v16h16V4zm-2 14H6V6h12v12zM8 8h8v2H8V8z"/></svg>
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" fill="black"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 4H4v16h16V4zm-2 14H6V6h12v12zM8 8h8v2H8V8z"/></svg>
         support@bharatvivaha.in
       </p>
-      <p>
-        <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M6.62 10.79a15.91 15.91 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.21c1.21.49 2.54.76 3.94.76a1 1 0 011 1V20a1 1 0 01-1 1C10.74 21 3 13.26 3 4a1 1 0 011-1h3.5a1 1 0 011 1c0 1.4.27 2.73.76 3.94a1 1 0 01-.21 1.11l-2.2 2.2z"/></svg>
+      <p style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;">
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" fill="black"><path d="M0 0h24v24H0z" fill="none"/><path d="M6.62 10.79a15.91 15.91 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.21c1.21.49 2.54.76 3.94.76a1 1 0 011 1V20a1 1 0 01-1 1C10.74 21 3 13.26 3 4a1 1 0 011-1h3.5a1 1 0 011 1c0 1.4.27 2.73.76 3.94a1 1 0 01-.21 1.11l-2.2 2.2z"/></svg>
         +91-98765 43210
       </p>
       <p>
-        <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" fill="white"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/></svg>
+        <svg class="icon" xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 24 24" fill="black"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5S10.62 6.5 12 6.5s2.5 1.12 2.5 2.5S13.38 11.5 12 11.5z"/></svg>
         Chennai, Tamil Nadu, India
       </p>
     </div>
 
     <!-- WeddingInfo Integration -->
     <div class="footer-column">
-      <h4>Wedding Services</h4>
-      <p>Planning your big day? Visit our sister site <strong><a href="https://www.weddinginfo.in" target="_blank" style="color:#fff; text-decoration:underline;">WeddingInfo.in</a></strong> for trusted services like:</p>
+      <h2>Wedding Services</h2>
+      <p>Planning your big day? Visit our sister site <strong><a href="https://www.weddinginfo.in" target="_blank" style="color: #b2002d; text-decoration:none;">WeddingInfo.in</a></strong> for trusted services like:</p>
       <ul>
         <li><a href="#" target="_blank">Marriage Halls</a></li>
         <li><a href="#" target="_blank">Mehendi Artists</a></li>
@@ -47,17 +62,17 @@
     </div>
   </div>
 
-  <div class="footer-bottom">
+  <div class="footer-bottom" style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; font-weight: 600;">
     &copy; 2025 BharatVivaha.in | All Rights Reserved.
   </div>
 </footer>
 
 <style>
   .footer {
-    background-color: #222;
-    color: #ccc;
+    background-color: #FFEFD5;
+    color: black;
     padding: 40px 20px 20px;
-    font-family: 'Segoe UI', sans-serif;
+    font-family: Georgia, 'Times New Roman', Times, serif;
   }
 
   .footer-container {
@@ -74,10 +89,12 @@
     min-width: 200px;
   }
 
-  .footer-column h4 {
-    color: #fff;
+  .footer-column h2 {
+    color: #b2002d;
     margin-bottom: 15px;
-    font-size: 18px;
+    font-weight: 600;
+    /* font-size: 22px; */
+    font-family: 'Parisienne', cursive;
   }
 
   .footer-column ul {
@@ -90,7 +107,7 @@
   }
 
   .footer-column ul li a {
-    color: #ccc;
+    color: black;
     text-decoration: none;
   }
 
