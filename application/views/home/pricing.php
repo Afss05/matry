aca9442632a8a919edb7793dc3bd0797acd850eb
Murga <sheikabu1605@gmail.com>
2025-06-08 00:10:29 +0530

    Update pricing.php

--- application/views/home/pricing.php
+++ application/views/home/pricing.php
@@ -1,192 +1,64 @@
+
 <!DOCTYPE html>
 <html dir="ltr" lang="en">
+
 <head>
-    <title>Bharat Vivaha Matrimony</title>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- css file -->
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets_index/css/bootstrap.min.css">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Best matrimony services in chennai | Top 10 matrimony services in chennai</title>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+    <meta name="description" content="Bharat Vivaha matrimony offer services like Best matrimony services in chennai, Top 10 matrimony services in chennai, best matrimony websites in chennai, Best matrimony services in tamilnadu, Top 10 matrimony services in tamilnadu, Bharat vivaha matrimony in chennai, Tamil Matrimony services in chennai, Telugu Matrimony services in chennai">
+    <meta name="keywords" content="Best matrimony services in chennai, Top 10 matrimony services in chennai, best matrimony websites in chennai, Best matrimony services in tamilnadu, Top 10 matrimony services in tamilnadu, Bharat vivaha matrimony in chennai, Tamil Matrimony services in chennai, Telugu Matrimony services in chennai">
+    <meta name="robots" CONTENT="all,index,follow"> 
+    <META NAME="revisit-after" CONTENT="3 days">
+    <!-- css file -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?php echo base_url(); ?>main/css/style.css">
 
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets_index/css/colors/default.css">
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets_index/css/style.css">
-<!-- Responsive stylesheet -->
-<link rel="stylesheet" href="<?php echo base_url(); ?>assets_index/css/responsive.css">
-<!-- Title -->
-<title></title>
-<!-- Favicon -->
-<link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" type="image/x-icon" />
-<link href="images/favicon.ico" sizes="128x128" rel="shortcut icon" />
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
 </head>
 <body>
-<div class="wrapper">
+    <?php echo $topmenu; ?>
 
-	
-	<header class="header-nav transparent">
-		<div class="container">
-		    <!-- Start Navigation -->
-		    <nav class="navbar navbar-default navbar-fixed  no-background bootsnav">
-		        <div class="container">
-		            <!-- Start Header Navigation -->
-		            <div class="navbar-header">
-		                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
-		                    <i class="fa fa-bars"></i>
-		                </button>
-		                <a class="navbar-brand ulockd-main-logo2" href="<?php echo base_url(); ?>">
-		                    <img src="<?php echo base_url(); ?>assets_index/images/header-logo2_back.png" class="logo logo-display hidden-md white-bg" alt="header-logo.png" height="90px">
-		                    <img src="<?php echo base_url(); ?>assets_index/images/header-logo2_back.png" class="logo logo-scrolled" alt="" height="90px">
-		                </a>
-		            </div>
-		            <!-- End Header Navigation -->
+    <section>
+        <div class="price-head">
+            <div class="pricing-content text-center  pt-5">
+                <p class="section-label">PRICING</p>
+                <h2 class="section-title">Get Started<br>Pick your Plan Now</h2>
+                <p class="section-subtext">
+                <!-- Lorem Ipsum is simply dummy text of the printing and typesetting industry. -->
+                </p>
+                <!-- <button class="pricing-btn">No credit card required</button> -->
+            </div>
+        </div>
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><g fill="#FFEFD5"><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 77 666.7 77 833.3 28 1000 28V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 70 666.7 70 833.3 16 1000 16V0H0Z" opacity=".5"></path><path d="M0 0v100c166.7 0 166.7-66 333.3-66S500 63 666.7 63 833.3 4 1000 4V0H0Z"></path></g></svg></main>
 
-		            <!-- Collect the nav links, forms, and other content for toggling -->
-					<?php echo $topmenu; ?>
-				 </div>
-		    </nav>
-		    <!-- End Navigation -->
-		</div>
-	</header>
-	
-	
-	<!-- Our Pricing Table -->
-	<section class="ulockd-pricing bg-dark">
-		<div class="container">
-			<div class="row">
-				<div class="col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3 text-center">
-					<div class="ulockd-main-title pt-2">
-						<h2 style="margin-top: 50px; text-shadow: 2px 2px #ff0000; color:black;">Our Pricing Table</h2>
-					
-					</div>
-				</div>
-			</div>
-<div class="row ulockd-mrgn1260">
-<?php if($this->session->flashdata('message')){     
-?>    
-<div class="alert alert-success success">
-<?php echo $this->session->flashdata('message') ?>                   
+    </section>
+
+    <!-- aleart -->
+<div class="row ">
+    <?php if($this->session->flashdata('message')) { ?>    
+    <div class="alert-box">
+        <?php echo $this->session->flashdata('message'); ?>                   
+    </div>
+    <?php } ?>    
+
+    <script>
+    $(document).ready(function(){
+        setTimeout(function(){
+            $(".alert-box").fadeOut("slow");
+        }, 3000); // 3 seconds = 3000 milliseconds
+    });
+</script>
+
+
 </div>
-<?php } ?>    
+    <section>
 
-			
-<?php 
-$i=0;$j=0;
-$PaymentType=$Amount=$PaidedValidy=$ProfileCounts="";
-if(isset($payment_details) && ($payment_details!="")){
-foreach($payment_details as $item){
+    </section>
 
-$PaymentType=$item->PaymentType;
-$Amount=$item->Amount;
-$ProfileCounts=$item->ProfileCounts;
-$PaidedValidy=$item->PaidedValidy;
-$id=$item->Id;
-
-$j++;
-?>
-<div class="col-xxs-12 col-xs-6 col-sm-6 col-md-3 ulockd-pad395" style="margin-bottom:50px;">
-<div class="ulockd-pricing-table text-center">
-<div class="ulocked-pricing-header">
-
-<h2 class="text-uppercase" style="font-family: Georgia, 'Times New Roman', Times, serif; text-shadow: 2px 2px white; font-size: 25px;"><?php echo $PaymentType; ?></h2>
-<p class="sub-title"><!--Monthly Plan--></p>
-</div>
-<div class="ulocked-pricing-details">
-<ul class="list-unstyled">
-<li> Profile View : <b><?php echo $ProfileCounts; ?></b>  </li>
-<li> Amount :  <b><?php echo $Amount; ?></b> </li>
-<li> Days : <b><?php echo $PaidedValidy; ?> </b> </li>
-
-
-</ul>
-<!--  -->
-<style>
-	.list-unstyled {
-		margin-top: 30px;
-		border: 2px solid white;
-		padding: 10px;
-		font-size: 16px;
-		box-shadow: -0px 1px 2px black inset;
-	}
-	.list-unstyled li  {
-		font-family: 'Times New Roman', Times, serif;
-		font-size: 18px;
-	}
-	.btn-block {
-		font-family: 'Times New Roman', Times, serif;
-	}
-</style>
-
-<?php 
-
-$userid = $this->session->userdata('logged_in'); 
-if($userid!=""){
-$userscrib=$this->Admin_model->usersubscribedplan($userid);
-$Paymentsubcrib="";
-if(count($userscrib)>0){
-foreach($userscrib as $item4){
-	$Paymentsubcrib=$item4->PaymentType;
-}}
-
-if($Paymentsubcrib==$id){
-?>
-<a  class="btn btn-lg btn-block ulockd-btn-thm2" style="    background-color:#303030;border-color: #303030;color: #ffffff;"><span> Subscribed</span></a>
-
-<?php }else{ ?>
-
-
-
-
-<a href="<?php echo base_url(); ?>payment/price_set/<?php echo $this->chsslibrary->encoder($id); ?>/<?php echo $this->chsslibrary->encoder($Amount); ?>" class="btn btn-lg btn-block ulockd-btn-thm2"><span> Choose Your Plan</span></a>
-
-
-<?php } }else{ ?>
-	
-
-<a href="<?php echo base_url(); ?>payment/price_set/<?php echo $this->chsslibrary->encoder($id); ?>/<?php echo $this->chsslibrary->encoder($Amount); ?>" class="btn btn-lg btn-block ulockd-btn-thm2"><span> Choose Your Plan </span></a>
-
-<?php } ?>
-</div>
-</div>
-</div>
-			
-<?php } } ?>
-		
-			
-			</div>
-		</div>
-	</section>
-
-
-	<!-- Our Footer -->
-	<?php echo $footer; ?>
-</div>
-<!-- Wrapper End -->
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/jquery-1.12.4.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/bootsnav.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/parallax.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/scrollto.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/jquery-scrolltofixed-min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/jquery.counterup.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/gallery.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/wow.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/slider.js"></script>
-
-<!--
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/video-player.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/jflickrfeed.min.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/jquery.barfiller.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/timepicker.js"></script>
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/tweetie.js"></script>
-<!-- Custom script for all pages 
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/color-switcher.js"></script> --> 
-<script type="text/javascript" src="<?php echo base_url(); ?>assets_index/js/script.js"></script>
 </body>
-
 </html>
