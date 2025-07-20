964a591fa43462450872a0c613bd082f5eea5920
Sheik Abdulla <sheikabu1605@gmail.com>
2025-07-20 18:09:12 +0530

    Update change_password.php

--- application/views/change_password.php
+++ application/views/change_password.php
@@ -43,32 +43,96 @@ body {
             
 
         <div class="" id="mydata">
-            <div class="col-lg-7 mx-auto">
+            <!-- <div class="col-lg-7 mx-auto"> -->
+            <div class="col-lg-7 ">
                 <div class="card border-0 m-3 rounded-4 shadow-none">
-                    <div class="card-header bg-primary text-white rounded-top-4">
+                    <div class="card-header p-3 bg-primary text-white rounded-top-4" style='font-family: "Charm", sans-serif; font-weight: 700;'>
                         <h5 class="mb-0"><i class="fa fa-key me-2"></i>Change Password</h5>
                     </div>
                     <div class="card-body bg-light">
                         <form role="form" action="<?php echo base_url(); ?>user/change_password_submit" method="post">
                             <?php if($this->session->flashdata('message')){ ?>
-                                <div class="alert alert-success success rounded-3">
+                                <div class="alert alert-success success rounded-3" id="success-alert" style='font-family: "Charm", sans-serif; font-weight: 700;'>
                                     <?php echo $this->session->flashdata('message') ?>
+                                    <span class="border-loader" id="border-loader"></span>
                                 </div>
+                                <style>
+                                    .border-loader {
+                                        display: inline-block;
+                                        width: 24px;
+                                        height: 24px;
+                                        border: 3px solid #fff;
+                                        border-top: 3px solid #198754;
+                                        border-radius: 50%;
+                                        animation: spin 1s linear infinite;
+                                        vertical-align: middle;
+                                        margin-left: 10px;
+                                    }
+                                    @keyframes spin {
+                                        0% { transform: rotate(0deg);}
+                                        100% { transform: rotate(360deg);}
+                                    }
+                                    label {
+                                        font-family: 'Poppins', sans-serif;
+                                        font-weight: 500;
+                                    }
+                                    input {
+                                        font-family: 'Poppins', sans-serif;
+                                        font-weight: 400;
+                                    }
+                                </style>
+                                <script>
+                                    setTimeout(function() {
+                                        var alert = document.getElementById('success-alert');
+                                        if (alert) {
+                                            alert.style.display = 'none';
+                                        }
+                                    }, 3000);
+                                </script>
                             <?php } ?>
-                            <div class="mb-3">
+                            <div class="mb-3 position-relative">
                                 <label class="form-label fw-semibold">Current Password</label>
-                                <input name="currentpassword" required type="password" placeholder="Enter current password" class="form-control rounded-3">
+                                <div class="input-group">
+                                    <input name="currentpassword" required type="password" placeholder="Enter current password" class="form-control rounded-3" id="currentpassword">
+                                    <span class="input-group-text bg-white border-0" style="cursor:pointer;" onclick="togglePassword('currentpassword', this)">
+                                        <i class="fa fa-eye-slash"></i>
+                                    </span>
+                                </div>
                             </div>
-                            <div class="mb-3">
+                            <div class="mb-3 position-relative">
                                 <label class="form-label fw-semibold">New Password</label>
-                                <input name="newpassword" required type="password" placeholder="Enter new password" class="form-control rounded-3">
+                                <div class="input-group">
+                                    <input name="newpassword" required type="password" placeholder="Enter new password" class="form-control rounded-3" id="newpassword">
+                                    <span class="input-group-text bg-white border-0" style="cursor:pointer;" onclick="togglePassword('newpassword', this)">
+                                        <i class="fa fa-eye-slash"></i>
+                                    </span>
+                                </div>
                             </div>
-                            <div class="mb-3">
+                            <div class="mb-3 position-relative">
                                 <label class="form-label fw-semibold">Confirm Password</label>
-                                <input name="retypepassword" required type="password" placeholder="Enter confirm password" class="form-control rounded-3">
+                                <div class="input-group">
+                                    <input name="retypepassword" required type="password" placeholder="Enter confirm password" class="form-control rounded-3" id="retypepassword">
+                                    <span class="input-group-text bg-white border-0" style="cursor:pointer;" onclick="togglePassword('retypepassword', this)">
+                                        <i class="fa fa-eye-slash"></i>
+                                    </span>
+                                </div>
                             </div>
-                            <div class="d-grid">
-                                <button class="btn btn-primary btn-lg rounded-3" type="submit">
+                            <script>
+                            function togglePassword(inputId, el) {
+                                var input = document.getElementById(inputId);
+                                var icon = el.querySelector('i');
+                                if (input.type === "password") {
+                                    input.type = "text";
+                                    icon.classList.remove('fa-eye-slash');
+                                    icon.classList.add('fa-eye');
+                                } else {
+                                    input.type = "password";
+                                    icon.classList.remove('fa-eye');
+                                    icon.classList.add('fa-eye-slash');
+                                }
+                            }
+                            </script>          <div class="d-grid">
+                                <button class="btn btn-primary btn-lg rounded-3" type="submit" style='font-family: "Charm", sans-serif; font-weight: 700;'>
                                     <strong>Submit</strong>
                                 </button>
                             </div>
